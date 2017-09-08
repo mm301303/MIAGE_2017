d892438661ba9cece1521e1d012d4fd73a04b233
max <maxime.moineau.dev@gmail.com>
2017-09-08 14:06:25 +0200

    update classes  2017-09-08

--- commit_and_push_everything.sh
+++ commit_and_push_everything.sh
@@ -1,7 +1,19 @@
-#!bin/bash
+#!/bin/bash
 
-git add ./*/*.mdown;
+d=$(date +%Y-%m-%d)
+
+#add classes here
+git add $0;
+git add COO/*.mdown SIMULATION\ DE\ GESTION/*.mdown;
+
+#crtl c ctrl v for pdf
+git add COO/*.pdf SIMULATION\ DE\ GESTION/*.pdf;
+
+
+echo "Update classes :";
+
 git status;
-git commit -m "update classes";
+git commit -m "update classes  $d";
 git push -u origin master;
-sl;
+
+echo "done";
