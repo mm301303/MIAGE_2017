a84f66b86d4536707c4546656fe34f741a587d6a
max <maxime.moineau.dev@gmail.com>
2017-09-11 15:07:15 +0200

    update classes  2017-09-11

--- commit_and_push_everything.sh
+++ commit_and_push_everything.sh
@@ -4,7 +4,7 @@ d=$(date +%Y-%m-%d)
 
 #add classes here
 git add $0;
-git add COO/*.mdown SIMULATION\ DE\ GESTION/*.mdown;
+git add COO/*.mdown SIMULATION\ DE\ GESTION/*.mdown HACKATON-SKEMA/*.mdown;
 
 #crtl c ctrl v for pdf
 git add COO/*.pdf SIMULATION\ DE\ GESTION/*.pdf;
