66a63ed4d95625f3e5a5776207f59ce9c53c37a8
Maxime M <maxime.moineau@free.fr>
2017-12-08 14:02:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- Système et réseau/TP3/Code/atable.c
+++ Système et réseau/TP3/Code/atable.c
@@ -2,19 +2,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
-#include <unistd.h>
 
 #define N 1000000
-#define M 10
+#define M 100
 
 int c[M];
-
-pthread_mutex_t mutexI = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutexJ = PTHREAD_MUTEX_INITIALIZER;
-
-
-
-//pthread_cond_t cond[M];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 /*****************************************/
@@ -26,48 +19,20 @@ pthread_mutex_t mutexJ = PTHREAD_MUTEX_INITIALIZER;
 // FONCTIONS A COMPLETER
 
 void enter_critical(int i, int j){
-  if(i!=j){
-    if(cond[i]==NULL) {
-      //cond[i] = PTHREAD_COND_INITIALIZER;
-    }else{ 
-      pthread_cond_wait (&cond[i], &mutexI);
+  int crit = pthread_mutex_lock(&mutex);
 
-    }
-    if(cond[j]==NULL) {
-      //cond[j] = PTHREAD_COND_INITIALIZER;
-    }else{
-      pthread_cond_wait (&cond[j], &mutexJ);
-    }
-
-    int critI = pthread_mutex_lock(&mutexI);
-    if(critI) perror("pthread_mutex_lock");
-    int critJ = pthread_mutex_lock(&mutexJ);
-    if(critJ) perror("pthread_mutex_lock");
-  }
+  if(crit) perror("pthread_mutex_lock");
 }
 
 void exit_critical(int i, int j){
-  if(i!=j){
-    //pthread_cond_signal (&cond[i]);
-    //pthread_cond_signal (&cond[j]); 
-
-
-    int critI = pthread_mutex_unlock(&mutexI);
-    if(critI) perror("pthread_mutex_unlock");
-    int critJ = pthread_mutex_unlock(&mutexJ);
-    if(critJ) perror("pthread_mutex_unlock");
-  }
+  int crit = pthread_mutex_unlock(&mutex);
+  if(crit) perror("pthread_mutex_lock");
 }
 
-void init_mutexes(){
-  for(int i=0; i<M; i++){
-    //cond[i] = PTHREAD_COND_INITIALIZER;
-  }
-}
+void init_mutexes(){}
 
 void destroy_mutexes(){
-  pthread_mutex_destroy(&mutexI);
-  pthread_mutex_destroy(&mutexJ);
+  pthread_mutex_destroy(&mutex);
 
 }
 
@@ -119,16 +84,19 @@ int main(int argc, char **argv){
   srand(time(NULL));
   
   int i;
-  for(i=0;i<M;i++) {
-    c[i] = i;
-  }
+  for(i=0;i<M;i++) c[i] = i;
 
   pthread_create(&t1,NULL,threadfun,"thread 1");
   pthread_create(&t2,NULL,threadfun,"thread 2");
   pthread_join(t1,NULL);
   pthread_join(t2,NULL);
 
-  for(i=0;i<M;i++) printf("c[%d] = %d\n",i,c[i]);
-  printf("Fin main.\n");
+  int sum = 0;
+
+  for(i=0;i<M;i++) {
+    printf("c[%d] = %d\n",i,c[i]);
+    sum+=c[i];
+  }
+  printf("Fin main.%d\n",sum);
 
 }
